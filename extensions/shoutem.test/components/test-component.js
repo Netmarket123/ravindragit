851ab4a08d3fc86088cf1248be3b4474d5d907f1
Vladimir Adamic <vladimir@shoutem.com>
2016-02-22 13:59:13 +0100

    Fix indentation on child component

--- extensions/shoutem.test/components/test-component.js
+++ extensions/shoutem.test/components/test-component.js
@@ -25,14 +25,14 @@ export default class TestComponent extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-      <Text>Welcome to React Native!</Text>
-      <Text style={styles.instructions}>
-      To get started, edit index.ios.js
-      </Text>
-      <Text style={styles.instructions}>
-      Press Cmd+R to reload,{'\n'}
-      Cmd+D or shake for dev menu
+        <Text>Welcome to React Native!</Text>
+        <Text style={styles.instructions}>
+        To get started, edit index.ios.js
         </Text>
+        <Text style={styles.instructions}>
+        Press Cmd+R to reload,{'\n'}
+        Cmd+D or shake for dev menu
+          </Text>
       </View>
     );
   }
