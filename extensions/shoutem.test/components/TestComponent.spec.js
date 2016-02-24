5624429076240793ecb8d3f48e869fe8b35e19fe
Luka Bracanovic <luka.bracanovic@fiveminutes.eu>
2016-02-24 17:58:29 +0100

    Update test component src and tests
    - Rename files
    - Use assert instead expect
    - SEEXT-52

--- extensions/shoutem.test/components/TestComponent.spec.js
+++ extensions/shoutem.test/components/TestComponent.spec.js
@@ -6,21 +6,21 @@
 
 import React, { Text } from 'react-native';
 import { shallow } from 'enzyme';
-import TestComponent from './test-component';
-import { expect } from 'chai';
+import TestComponent from './TestComponent.jsx';
+import { assert } from 'chai';
 
 describe('<TestComponent />', () => {
   it('should render stuff', () => {
     const wrapper = shallow(<TestComponent />);
 
     // "dom like" testing
-    expect(wrapper.length).to.equal(1);
+    assert.equal(wrapper.length, 1);
 
-    // TODO: Braco - find out how to test components with style? or other attributes
-    expect(wrapper.contains(<Text>Welcome to React Native!</Text>)).to.equal(true);
+    // TODO find out how to test components with style? or other attributes
+    assert.isTrue(wrapper.contains(<Text>Welcome to React Native!</Text>));
 
     // testing component public method
-    expect(wrapper.instance().testMethod()).to.equal(5);
+    assert.equal(wrapper.instance().testMethod(), 5);
   });
 });
 
---
+++ extensions/shoutem.test/components/_tests_/TestComponent.spec.js
@@ -0,0 +1,14 @@
+/**
+ *
+ * Test example in _test_ folder
+ *
+ */
+
+import { TestAtComponentLoaded } from '../TestComponent.spec';
+import { assert } from 'chai';
+
+describe('test in _tests_ folder', () => {
+    it('test at component should be true', () => {
+        assert.isTrue(TestAtComponentLoaded);
+    });
+});
