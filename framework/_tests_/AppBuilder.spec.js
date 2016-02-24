0212f516f30fa8e8e1068e870c72deb78de6ddb9
Željko Rumenjak <zeljko@shoutem.com>
2016-02-24 19:30:58 +0100

    Add some more tests for the AppBuilder.

--- framework/_tests_/AppBuilder.spec.js
+++ framework/_tests_/AppBuilder.spec.js
@@ -1,25 +1,40 @@
 import React, {
-  View
+  Component,
 } from 'react-native';
 import { assert } from 'chai';
 import { shallow } from 'enzyme';
 
 import AppBuilder from '../AppBuilder.js';
 
+class Screen extends Component {
+  render() {
+    return (<Text>This is a screen!</Text>);
+  }
+}
+
+function emptyReducer() {
+  return {};
+}
+
 function getDefaultExtensions() {
   return {
     default: {
-      reducer() {
-        return {};
-      },
+      reducer: emptyReducer,
     },
   };
 }
 
 function getDefaultScreens() {
   return {
-    default: View,
+    default: Screen,
   };
+}
+
+function assertValidApp(App) {
+  assert.isOk(App, 'The app class is invalid');
+
+  const wrapper = shallow(<App />);
+  assert.lengthOf(wrapper.children, 1, 'The app that was created could not be rendered');
 }
 
 describe('AppBuilder', function () {
@@ -42,8 +57,7 @@ describe('AppBuilder', function () {
       .setScreens(getDefaultScreens())
       .build();
 
-    const wrapper = shallow(<App />);
-    assert.lengthOf(wrapper.children, 1, 'The app that was created could not be rendered');
+    assertValidApp(App);
   });
 
   it('must not create an app without any extensions', function() {
@@ -60,5 +74,52 @@ describe('AppBuilder', function () {
       .setScreens({});
 
     assert.throws(builder.build.bind(builder), 'The app without any screens cannot be created.');
+  });
+
+  it('can create an app with multiple extensions', function() {
+    const App = new AppBuilder()
+      .setExtensions({
+        extension1: {
+          reducer: emptyReducer,
+        },
+        extension2: {
+          reducer: emptyReducer,
+        },
+      })
+      .setScreens(getDefaultScreens())
+      .build();
+
+    assertValidApp(App);
+  });
+
+  it('can create an app with multiple screens', function() {
+    const App = new AppBuilder()
+      .setExtensions(getDefaultExtensions())
+      .setScreens({
+        screen1: Screen,
+        screen2: Screen,
+        screen3: Screen,
+      })
+      .build();
+
+    assertValidApp(App);
+  });
+
+  it('can create multiple apps', function() {
+    const builder = new AppBuilder();
+
+    const App1 = builder.setExtensions(getDefaultExtensions())
+      .setScreens(getDefaultScreens())
+      .build();
+
+    const App2 = builder.setScreens({
+        screen1: Screen,
+        screen2: Screen,
+      })
+      .build();
+
+    assertValidApp(App1);
+    assertValidApp(App2);
+    assert.notStrictEqual(App1, App2, 'The Builder returned the same app class');
   });
 });
