1692162a07765211660e0aa4ffb39ff04a5cb639
Vladimir Adamic <vladimir@shoutem.com>
2016-02-25 13:50:16 +0100

    Start CodePush sync on componentDidMount
    - SEEXT-214

--- bin/index.js
+++ bin/index.js
@@ -6,6 +6,8 @@ import React, {
     View,
 } from 'react-native';
 
+import codePush from 'react-native-code-push';
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -26,6 +28,12 @@ const styles = StyleSheet.create({
 });
 
 class ShoutemApp extends Component {
+  componentDidMount() {
+    // Active update, which lets the end user know
+    // about each update, and displays it to them
+    // immediately after downloading it
+    codePush.sync({ updateDialog: true, installMode: codePush.InstallMode.IMMEDIATE });
+  }
   render() {
     return (
           <View style={styles.container}>
