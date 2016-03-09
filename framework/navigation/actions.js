472aad552e2103dc84e79dc5b5770a2730bc6965
Željko Rumenjak <zeljko@shoutem.com>
2016-03-09 20:40:31 +0100

    Add tests for the navigation reducer and actions.

--- framework/navigation/actions.js
+++ framework/navigation/actions.js
@@ -1,3 +1,19 @@
+/**
+ * This file contains the reducer and action creators for
+ * all ScreenNavigators in the application. Each navigator
+ * is identified by its name, and has a corresponding key
+ * in the state.
+ *
+ * The navigation flow works as follows:
+ * 1. The navigation action is dispatched
+ * 2. The navigation reducer records this action in the state
+ * 3. The ScreenNavigator receives the store change event
+ * 4. The ScreenNavigator performs the action
+ * 5. The ScreenNavigator triggers the NAVIGATION_ACTION_PERFORMED action
+ * 6. The navigation reducer clears the performed action from the state
+ * 7. The navigation reducer records the new navigation stack of the navigator
+ */
+
 export const NAVIGATE_TO = 'shoutem.core.navigation.NAVIGATE_TO';
 export const NAVIGATE_BACK = 'shoutem.core.navigation.NAVIGATE_BACK';
 
@@ -9,11 +25,18 @@ function navigatorReducer(state = {}, action) {
   switch (action.type) {
     case NAVIGATE_TO:
     case NAVIGATE_BACK:
+      // We only save all valid actions in the
+      // state here, the ScreenNavigator component
+      // will actually perform it when it receives
+      // the store change.
       return {
         ...state,
         action,
       };
     case NAVIGATION_ACTION_PERFORMED:
+      // The navigation action was performed,
+      // so we now clear it from the state, and
+      // record the new navigation stack.
       return {
         ...state,
         stack: action.navigationStack,
@@ -27,6 +50,8 @@ function navigatorReducer(state = {}, action) {
 export default function reducer(state = {}, action) {
   const navigatorName = action.navigator;
   if (!navigatorName) {
+    // We can only handle navigation actions that
+    // target a specific navigator.
     return state;
   }
 
