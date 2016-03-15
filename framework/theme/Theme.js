71381b6594ed818ca1eb4b2a1c1bbf12f35c8c9d
Luka Bracanovic <luka.bracanovic@fiveminutes.eu>
2016-03-15 18:49:08 +0100

    Change Theme implementation
    - Add resolveComponentStyle
    - Make other methods private
    - Support nested style
    - SEEXT-246

--- framework/theme/Theme.js
+++ framework/theme/Theme.js
@@ -1,50 +1,83 @@
-import { StyleSheet, PropTypes } from 'react-native';
-import { mergeStyle } from './ThemeHelpers';
+import { PropTypes } from 'react-native';
+import { mergePureStyle, mergeNestedStyle, separatePureAndNestedStyle } from './ThemeHelpers';
+
+/**
+ * Pure style is style which can be optimized with StyleSheet.create and
+ * is used to style basic react components.
+ * Nested style we pass to children.
+ */
 
 // Constants
 const GLOBAL_STYLE = 'global';
 
 // Privates
-const themeStyleSymbol = Symbol('themeStyle');
-const themeCachedStyleSymbol = Symbol('themeCachedStyle');
+const themeStyle = Symbol('themeStyle');
+const themeCachedStyle = Symbol('themeCachedStyle');
+const getComponentThemeStyle = Symbol('getComponentThemeStyle');
+const createComponentThemeStyle = Symbol('createComponentThemeStyle');
 
 /**
  *  Theme instance holds app theme and provides methods to create and update component style.
  *  Component static style is optimized and with StyleSheet.create and cached for reuse.
  */
 export default class Theme {
-  constructor(themeStyle) {
-    this[themeStyleSymbol] = themeStyle;
-    this[themeCachedStyleSymbol] = {};
+  constructor(themeStyles) {
+    this[themeStyle] = themeStyles;
+    this[themeCachedStyle] = {};
   }
-  createComponentStyle(styleName, style, customStyle) {
-    const componentThemeStyle = this.createThemeComponentReactStyle(styleName, style);
+  resolveComponentStyle(styleName, style, customStyle) {
+    let componentThemeStyle = this[getComponentThemeStyle](styleName);
 
-    return mergeStyle(componentThemeStyle, customStyle);
+    if (!componentThemeStyle) {
+      componentThemeStyle = this[createComponentThemeStyle](styleName, style);
+    }
+
+    const {
+      pureStyle: componentPureThemeStyle,
+      nestedStyle: componentNestedThemeStyle,
+    } = componentThemeStyle;
+    const {
+      pureStyle: customPureStyle,
+      nestedStyle: customNestedStyle,
+    } = separatePureAndNestedStyle(customStyle);
+
+    const componentPureStyle = mergePureStyle(componentPureThemeStyle, customPureStyle);
+    const componentNestedStyle = mergeNestedStyle(componentNestedThemeStyle, customNestedStyle);
+
+    return { ...componentPureStyle, ...componentNestedStyle };
   }
-  updateComponentStyle(styleName, customStyle) {
-    const componentThemeStyle = this.getComponentStyle(styleName);
+  [createComponentThemeStyle](styleName, style) {
+    // TODO:Braco - include everything that needs to be included
+    const globalThemeStyle = this[themeStyle][GLOBAL_STYLE];
+    const {
+      pureStyle: componentPureThemeStyle,
+      nestedStyle: componentNestedThemeStyle,
+    } = separatePureAndNestedStyle(this[themeStyle][styleName]);
+    const {
+      pureStyle: componentPureStyle,
+      nestedStyle: componentNestedStyle,
+    } = separatePureAndNestedStyle(style);
 
-    return mergeStyle(componentThemeStyle, customStyle);
+    const componentThemeStyle = mergePureStyle(componentPureStyle, componentPureThemeStyle);
+
+    /**
+     * globalThemeStyle is always pure
+     * Merging only common styles, not all global theme style with component style
+     */
+    const compPureStaticStyle = mergePureStyle(globalThemeStyle, componentThemeStyle, false);
+    const compNestedStaticStyle = mergeNestedStyle(componentNestedStyle, componentNestedThemeStyle);
+
+    this[themeCachedStyle][styleName] = {
+      pureStyle: compPureStaticStyle,
+      nestedStyle: compNestedStaticStyle,
+    };
+    return this[themeCachedStyle][styleName];
   }
-  createThemeComponentReactStyle(styleName, style) {
-    const componentCustomThemeStyle = this[themeStyleSymbol][styleName];
-    const globalThemeStyle = this[themeStyleSymbol][GLOBAL_STYLE];
-
-    const componentThemeStyle = mergeStyle(globalThemeStyle, componentCustomThemeStyle);
-    const componentStaticStyle = mergeStyle(style, componentThemeStyle);
-
-    this[themeCachedStyleSymbol][styleName] = StyleSheet.create(componentStaticStyle);
-    return this[themeCachedStyleSymbol][styleName];
-  }
-  getComponentStyle(styleName) {
-    return this[themeCachedStyleSymbol][styleName];
+  [getComponentThemeStyle](styleName) {
+    return this[themeCachedStyle][styleName];
   }
 }
 
 export const ThemeShape = PropTypes.shape({
-  createComponentStyle: PropTypes.func.isRequired,
-  createThemeComponentReactStyle: PropTypes.func.isRequired,
-  getComponentStyle: PropTypes.func.isRequired,
-  updateComponentStyle: PropTypes.func.isRequired,
+  resolveComponentStyle: PropTypes.func.isRequired,
 });
