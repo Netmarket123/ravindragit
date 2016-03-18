2591d996f2e9693d38851382f3861b737243ade2
Luka Bracanovic <luka.bracanovic@fiveminutes.eu>
2016-03-18 17:40:37 +0100

    Create gannettNews extensions
    - SEEXT-39

---
+++ extensions/shoutem.gannettNews/index.js
@@ -0,0 +1,11 @@
+import GannettListScreen from './screens/GanetListScreen';
+import GannettDetailsScreen from './screens/GannettDetailsScreen';
+import gannettItems from './mocks/ganetItems';
+import customTheme from './mocks/theme';
+
+export {
+  GannettListScreen,
+  GannettDetailsScreen,
+  gannettItems as mockedItems,
+  customTheme as mockedTheme,
+};
---
+++ extensions/shoutem.gannettNews/mocks/ganetItems.js
@@ -0,0 +1,142 @@
+export default [
+  {
+    id: 1,
+    description: 'Featured item or in this case first item',
+    source: 'USA Today',
+    date: '2 hours ago',
+    image: require('../assets/items/first.png'),
+    details: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam hendrerit pretium
+     ligula, vitae mattis mauris. Suspendisse turpis lectus, ullamcorper id maximus vel, varius
+     laoreet ligula. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere
+     cubilia Curae; Curabitur non vehicula tellus. Donec id massa ut risus placerat mattis
+    sed at tellus. Nunc id nisl blandit, vehicula velit id, ultrices lacus. Morbi id fringilla est.
+     Phasellus quis auctor nulla. Nunc risus sapien, accumsan eu diam at, tincidunt volutpat ex.
+     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed a dolor porta, pulvinar enim non,
+      molestie est. Sed feugiat consequat sapien, sit amet maximus leo posuere pharetra. Integer
+      tempor magna elementum libero semper aliquet. Vestibulum ac quam at orci vestibulum vulputate
+       vitae ut quam.
+
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam hendrerit pretium
+     ligula, vitae mattis mauris. Suspendisse turpis lectus, ullamcorper id maximus vel, varius
+     laoreet ligula. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere
+     cubilia Curae; Curabitur non vehicula tellus. Donec id massa ut risus placerat mattis
+    sed at tellus. Nunc id nisl blandit, vehicula velit id, ultrices lacus. Morbi id fringilla est.
+     Phasellus quis auctor nulla. Nunc risus sapien, accumsan eu diam at, tincidunt volutpat ex.
+     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed a dolor porta, pulvinar enim non,
+      molestie est. Sed feugiat consequat sapien, sit amet maximus leo posuere pharetra. Integer
+      tempor magna elementum libero semper aliquet. Vestibulum ac quam at orci vestibulum vulputate
+       vitae ut quam.`,
+  },
+  {
+    id: 2,
+    description: 'A really really long item description',
+    source: 'USA Today',
+    date: '3 hours ago',
+    image: require('../assets/items/first_1.png'),
+    details: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam hendrerit pretium
+     ligula, vitae mattis mauris. Suspendisse turpis lectus, ullamcorper id maximus vel, varius
+     laoreet ligula. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere
+     cubilia Curae; Curabitur non vehicula tellus. Donec id massa ut risus placerat mattis
+    sed at tellus. Nunc id nisl blandit, vehicula velit id, ultrices lacus. Morbi id fringilla est.
+     Phasellus quis auctor nulla. Nunc risus sapien, accumsan eu diam at, tincidunt volutpat ex.
+     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed a dolor porta, pulvinar enim non,
+      molestie est. Sed feugiat consequat sapien, sit amet maximus leo posuere pharetra. Integer
+      tempor magna elementum libero semper aliquet. Vestibulum ac quam at orci vestibulum vulputate
+       vitae ut quam.
+
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam hendrerit pretium
+     ligula, vitae mattis mauris. Suspendisse turpis lectus, ullamcorper id maximus vel, varius
+     laoreet ligula. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere
+     cubilia Curae; Curabitur non vehicula tellus. Donec id massa ut risus placerat mattis
+    sed at tellus. Nunc id nisl blandit, vehicula velit id, ultrices lacus. Morbi id fringilla est.
+     Phasellus quis auctor nulla. Nunc risus sapien, accumsan eu diam at, tincidunt volutpat ex.
+     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed a dolor porta, pulvinar enim non,
+      molestie est. Sed feugiat consequat sapien, sit amet maximus leo posuere pharetra. Integer
+      tempor magna elementum libero semper aliquet. Vestibulum ac quam at orci vestibulum vulputate
+       vitae ut quam.`,
+  },
+  {
+    id: 3,
+    description: 'Some item description',
+    source: 'USA Today',
+    date: '4 hours ago',
+    image: require('../assets/items/second.png'),
+    details: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam hendrerit pretium
+     ligula, vitae mattis mauris. Suspendisse turpis lectus, ullamcorper id maximus vel, varius
+     laoreet ligula. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere
+     cubilia Curae; Curabitur non vehicula tellus. Donec id massa ut risus placerat mattis
+    sed at tellus. Nunc id nisl blandit, vehicula velit id, ultrices lacus. Morbi id fringilla est.
+     Phasellus quis auctor nulla. Nunc risus sapien, accumsan eu diam at, tincidunt volutpat ex.
+     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed a dolor porta, pulvinar enim non,
+      molestie est. Sed feugiat consequat sapien, sit amet maximus leo posuere pharetra. Integer
+      tempor magna elementum libero semper aliquet. Vestibulum ac quam at orci vestibulum vulputate
+       vitae ut quam.
+
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam hendrerit pretium
+     ligula, vitae mattis mauris. Suspendisse turpis lectus, ullamcorper id maximus vel, varius
+     laoreet ligula. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere
+     cubilia Curae; Curabitur non vehicula tellus. Donec id massa ut risus placerat mattis
+    sed at tellus. Nunc id nisl blandit, vehicula velit id, ultrices lacus. Morbi id fringilla est.
+     Phasellus quis auctor nulla. Nunc risus sapien, accumsan eu diam at, tincidunt volutpat ex.
+     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed a dolor porta, pulvinar enim non,
+      molestie est. Sed feugiat consequat sapien, sit amet maximus leo posuere pharetra. Integer
+      tempor magna elementum libero semper aliquet. Vestibulum ac quam at orci vestibulum vulputate
+       vitae ut quam.`,
+  },
+  {
+    id: 4,
+    description: 'Some other item other description',
+    source: 'USA Today',
+    date: '5 hours ago',
+    image: require('../assets/items/third.png'),
+    details: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam hendrerit pretium
+     ligula, vitae mattis mauris. Suspendisse turpis lectus, ullamcorper id maximus vel, varius
+     laoreet ligula. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere
+     cubilia Curae; Curabitur non vehicula tellus. Donec id massa ut risus placerat mattis
+    sed at tellus. Nunc id nisl blandit, vehicula velit id, ultrices lacus. Morbi id fringilla est.
+     Phasellus quis auctor nulla. Nunc risus sapien, accumsan eu diam at, tincidunt volutpat ex.
+     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed a dolor porta, pulvinar enim non,
+      molestie est. Sed feugiat consequat sapien, sit amet maximus leo posuere pharetra. Integer
+      tempor magna elementum libero semper aliquet. Vestibulum ac quam at orci vestibulum vulputate
+       vitae ut quam.
+
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam hendrerit pretium
+     ligula, vitae mattis mauris. Suspendisse turpis lectus, ullamcorper id maximus vel, varius
+     laoreet ligula. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere
+     cubilia Curae; Curabitur non vehicula tellus. Donec id massa ut risus placerat mattis
+    sed at tellus. Nunc id nisl blandit, vehicula velit id, ultrices lacus. Morbi id fringilla est.
+     Phasellus quis auctor nulla. Nunc risus sapien, accumsan eu diam at, tincidunt volutpat ex.
+     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed a dolor porta, pulvinar enim non,
+      molestie est. Sed feugiat consequat sapien, sit amet maximus leo posuere pharetra. Integer
+      tempor magna elementum libero semper aliquet. Vestibulum ac quam at orci vestibulum vulputate
+       vitae ut quam.`,
+  },
+  {
+    id: 5,
+    description: 'Short description',
+    source: 'USA Today',
+    date: 'Day ago',
+    image: require('../assets/items/fourth.png'),
+    details: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam hendrerit pretium
+     ligula, vitae mattis mauris. Suspendisse turpis lectus, ullamcorper id maximus vel, varius
+     laoreet ligula. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere
+     cubilia Curae; Curabitur non vehicula tellus. Donec id massa ut risus placerat mattis
+    sed at tellus. Nunc id nisl blandit, vehicula velit id, ultrices lacus. Morbi id fringilla est.
+     Phasellus quis auctor nulla. Nunc risus sapien, accumsan eu diam at, tincidunt volutpat ex.
+     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed a dolor porta, pulvinar enim non,
+      molestie est. Sed feugiat consequat sapien, sit amet maximus leo posuere pharetra. Integer
+      tempor magna elementum libero semper aliquet. Vestibulum ac quam at orci vestibulum vulputate
+       vitae ut quam.
+
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam hendrerit pretium
+     ligula, vitae mattis mauris. Suspendisse turpis lectus, ullamcorper id maximus vel, varius
+     laoreet ligula. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere
+     cubilia Curae; Curabitur non vehicula tellus. Donec id massa ut risus placerat mattis
+    sed at tellus. Nunc id nisl blandit, vehicula velit id, ultrices lacus. Morbi id fringilla est.
+     Phasellus quis auctor nulla. Nunc risus sapien, accumsan eu diam at, tincidunt volutpat ex.
+     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed a dolor porta, pulvinar enim non,
+      molestie est. Sed feugiat consequat sapien, sit amet maximus leo posuere pharetra. Integer
+      tempor magna elementum libero semper aliquet. Vestibulum ac quam at orci vestibulum vulputate
+       vitae ut quam.`,
+  },
+];
