452ce7ef46b20f7e22b516355d819be51490f7eb
Luka Bracanovic <luka.bracanovic@fiveminutes.eu>
2016-03-21 21:08:55 +0100

    Theme improvements
    - Rename StyleConnector to connectStyle
    - Add index.js to theme to export publics

---
+++ framework/theme/index.js
@@ -0,0 +1,11 @@
+import connectStyle from './connectStyle';
+import { INCLUDE } from './StyleIncluder';
+import StyleProvider from './StyleProvider.jsx';
+import Theme from './Theme';
+
+export {
+  connectStyle,
+  INCLUDE,
+  StyleProvider,
+  Theme,
+};
