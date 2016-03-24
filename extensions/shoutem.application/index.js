b7b0cbae2b1a7ed14b129e400d90580d404616bd
Ivan Vuković <3.14wee@gmail.com>
2016-03-24 18:34:08 +0100

    Add execute shortcut middleware

--- extensions/shoutem.application/index.js
+++ extensions/shoutem.application/index.js
@@ -1,2 +1,29 @@
 import configuration from './configuration';
-export { configuration };
+import * as actions from './actions';
+import { createExecuteShortcutMiddleware } from './middleware';
+export {
+  configuration,
+  actions,
+};
+
+let appActions = {};
+
+export function appWillMount(appContext) {
+  const extensions = appContext.extensions;
+  appActions = Object.keys(extensions).reduce((prevResult, key) => {
+    const extension = extensions[key];
+    let result = prevResult;
+    if (extension.actions) {
+      result = {
+        ...prevResult,
+        [key]: extension.actions,
+      };
+    }
+
+    return result;
+  }, {});
+}
+
+export const middleware = [
+  createExecuteShortcutMiddleware(appActions),
+];
