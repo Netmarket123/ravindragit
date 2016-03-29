10f2404f78b3b9d71d122e6d153655139b6fd9eb
Vladimir Adamic <vladimir@shoutem.com>
2016-03-29 14:23:25 +0200

    Document denormalizer, bind method in constructor
    - SEEXT-267

--- extensions/shoutem.homescreen/actions/Denormalizer.js
+++ extensions/shoutem.homescreen/actions/Denormalizer.js
@@ -4,11 +4,19 @@ function extractValuesFromObject(object) {
   return _.reduce(object, (acc, val) => Object.assign({}, acc, val), {});
 }
 
+/**
+ * Returns provided data in denormalized form
+ */
 export default class Denormalizer {
   constructor(normalizedData) {
     this.normalizedData = normalizedData;
+    this.getDenormalizedItem = this.getDenormalizedItem.bind(this);
   }
 
+  /**
+   * finds an item by type and id from normalized data array and denormalizes it
+   * @returns {} - a denormalized item
+   */
   getDenormalizedItem(targetDescriptor) {
     const { type, id } = targetDescriptor;
     const normalizedItem = this.normalizedData.find(item => item.type === type && item.id === id);
@@ -16,7 +24,7 @@ export default class Denormalizer {
     const { attributes, relationships } = normalizedItem;
 
     const extractedRelationshipData = _.map(relationships, (value, key) => ({
-      [key]: value.data.map(this.getDenormalizedItem.bind(this)),
+      [key]: value.data.map(this.getDenormalizedItem),
     }));
 
     return Object.assign({}, attributes, extractValuesFromObject(extractedRelationshipData));
--- extensions/shoutem.homescreen/actions/OpenHomeScreenAction.js
+++ extensions/shoutem.homescreen/actions/OpenHomeScreenAction.js
@@ -4,11 +4,9 @@ export default function createOpenHomeScreenAction(navigateTo) {
   return (settings, state) => {
     const layout = settings.homeScreen.layout;
     const children = settings.homeScreen.children;
-    const childrenIds = children.map(child => child.id);
-    const shortcutDescriptors = state.filter(shortcut => childrenIds.indexOf(shortcut.id) > -1);
 
     const denormalizer = new Denormalizer(state);
-    const shortcuts = shortcutDescriptors.map(denormalizer.getDenormalizedItem.bind(denormalizer));
+    const shortcuts = children.map(denormalizer.getDenormalizedItem);
 
     navigateTo({ screen: 'shoutem.homeScreen.HomeScreen', props: { layout, shortcuts } });
   };
