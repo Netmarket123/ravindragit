dfcf9577429d873e70efcaacaa316ea6b1e18c92
Vladimir Adamic <vladimir@shoutem.com>
2016-03-30 15:06:16 +0200

    Use configuration included data

--- extensions/shoutem.homescreen/actions.js
+++ extensions/shoutem.homescreen/actions.js
@@ -3,7 +3,7 @@ import Denormalizer from './Denormalizer';
 export default function createOpenHomeScreenAction(navigateTo) {
   return (settings, children, state) => {
     const homeScreen = settings.homeScreen;
-    const denormalizer = new Denormalizer(state);
+    const denormalizer = new Denormalizer(state['shoutem.application'].configuration.included);
     const shortcuts = children.map(denormalizer.getDenormalizedItem);
 
     const route = {
