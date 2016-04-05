c9995c74dfdcb95c1e8d2c7f636c92582a8a883b
Vladimir Adamic <vladimir@shoutem.com>
2016-04-05 14:23:33 +0200

    Add Scaler decreased screen height test screnario
    - SEEXT-267

--- extensions/shoutem.homescreen/_tests_/Scaler.spec.js
+++ extensions/shoutem.homescreen/_tests_/Scaler.spec.js
@@ -94,4 +94,57 @@ describe('Scaler', () => {
       });
     });
   });
+
+  describe('screen height is half the height of the original layout', () => {
+    const testScreenSize = {
+      width: 1080,
+      height: 960,
+    };
+
+    describe('scalingStrategy is horizontal', () => {
+      const scalingStrategy = 'horizontal';
+      const scaler = new Scaler(scalingStrategy, testScreenSize, layoutDimension);
+
+      describe('getRatio', () => {
+        it('returns the ratio of 1', () => {
+          assert.equal(scaler.getRatio(), 1, 'ratio is not correct');
+        });
+      });
+
+      describe('scale', () => {
+        const expected = {
+          width: 150,
+          height: 150,
+        };
+
+        it('returns object that is equal to the original one', () => {
+          assert.deepEqual(scaler.scale(testObjectToResize), expected,
+                       'object has not been resized properly');
+        });
+      });
+    });
+
+    describe('scalingStrategy is vertical', () => {
+      const scalingStrategy = 'vertical';
+      const scaler = new Scaler(scalingStrategy, testScreenSize, layoutDimension);
+
+      describe('getRatio', () => {
+        it('returns the ratio of 0.5', () => {
+          assert.equal(scaler.getRatio(), 0.5, 'ratio is not correct');
+        });
+      });
+
+      describe('scale', () => {
+        const expected = {
+          width: 75,
+          height: 75,
+        };
+
+        it('returns object that is half the size of to the original one', () => {
+          assert.deepEqual(scaler.scale(testObjectToResize), expected,
+                       'object has not been resized properly');
+        });
+      });
+    });
+  });
 });
