50a41d32eda8af9111627c1d20f52a338fe7a264
Ivan Vuković <3.14wee@gmail.com>
2016-04-14 17:22:18 +0200

    Support deep link on iOS

--- bin/ios/ShoutemApp/AppDelegate.m
+++ bin/ios/ShoutemApp/AppDelegate.m
@@ -11,6 +11,8 @@
 
 #import "RCTRootView.h"
 
+#import "RCTLinkingManager.h"
+
 #import "CodePush.h"
 
 @implementation AppDelegate
@@ -59,6 +61,13 @@
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
   return YES;
+}
+
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url
+  sourceApplication:(NSString *)sourceApplication annotation:(id)annotation
+{
+  return [RCTLinkingManager application:application openURL:url
+                      sourceApplication:sourceApplication annotation:annotation];
 }
 
 @end
