cc68700d1f0653a1a8580ffc483e4aa6f4007ad3
Vladimir Adamic <vladimir@shoutem.com>
2016-04-19 14:55:30 +0200

    Add documentation to Video component
    - Add propTypes
    - SEEXT-406

--- extensions/shoutem.ui/components/Video/Video.js
+++ extensions/shoutem.ui/components/Video/Video.js
@@ -1,10 +1,18 @@
 import React, {
   View,
-  Component,
   StyleSheet,
+  PropTypes,
 } from 'react-native';
 
 import NativeVideo from './NativeVideo';
+
+const propTypes = {
+  width: PropTypes.number,
+  height: PropTypes.number,
+  source: PropTypes.shape({
+    uri: PropTypes.string,
+  }),
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -12,20 +20,30 @@ const styles = StyleSheet.create({
   },
 });
 
-class Video extends Component {
-  render() {
-    const { width, height } = this.props;
-
-    return (
-      <View style={[styles.container, { width, height }]}>
-        <NativeVideo
-          source={this.props.source}
-          width={this.props.width}
-          height={this.props.height}
-        />
-      </View>
-    );
-  }
+/**
+ * Renders a Video based on the source type
+ * Currently only direct links are supported
+ *
+ * TODO(Vladimir): support YouTube and Vimeo source types
+ * TODO(Vladimir): display a play button and a poster overlay
+ *
+ * @returns {*}
+ */
+export default function Video({
+  width,
+  height,
+  source,
+}) {
+  return (
+    <View style={[styles.container, { width, height }]}>
+      <NativeVideo
+        source={source}
+        width={width}
+        height={height}
+      />
+    </View>
+  );
 }
 
-export default Video;
+Video.propTypes = propTypes;
+
