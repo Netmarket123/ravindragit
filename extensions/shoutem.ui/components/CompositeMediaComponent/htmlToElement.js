629e70fbaa34da20764f96d0f3d95132ebe8d8cf
Vladimir Adamic <vladimir@shoutem.com>
2016-04-28 18:51:07 +0200

    Add containsAttachment algorithm

--- extensions/shoutem.ui/components/CompositeMediaComponent/htmlToElement.js
+++ extensions/shoutem.ui/components/CompositeMediaComponent/htmlToElement.js
@@ -23,6 +23,11 @@ function decodeHTML(string) {
   return entities.decodeHTML(string);
 }
 
+function containsAttachment(node) {
+  return node.name === 'attachment'
+    || (node.children && node.children.some((childNode) => containsAttachment(childNode)));
+}
+
 export default class HtmlRenderer {
   constructor(tagTransformers) {
     this.tagTransformers = tagTransformers;
@@ -54,31 +59,33 @@ export default class HtmlRenderer {
           );
         }
 
+        const ContainerElement = containsAttachment(node) ? View : Text;
+
         if (node.type === 'tag') {
           const mediaTag = renderTag(node, index);
 
           if (node.name === 'pre') {
             return (
-              <View key={index} >
+              <ContainerElement key={index} >
                 <Text>{LINE_BREAK}</Text>
                 {domToElement(node.children, node)}
-              </View>
+              </ContainerElement>
             );
           } else if (node.name === 'a') {
             return domToElement(node.children, node);
           } else if (node.name === 'li') {
             return (
-              <View key={index} >
+              <ContainerElement key={index} >
                 <Text>{BULLET}</Text>
                 {domToElement(node.children, node)}
-              </View>
+              </ContainerElement>
             );
           } else if (node.name === 'br') {
             return (
-              <View key={index} style={{width: 330, flexDirection: 'row', flexWrap: 'wrap'}} >
+              <ContainerElement key={index} style={{ width: 330, flexDirection: 'row', flexWrap: 'wrap' }} >
                 {domToElement(node.children, node)}
                 <Text>{LINE_BREAK}</Text>
-              </View>
+              </ContainerElement>
             );
           } else if (node.name === 'p') {
             let paragraphBreak = null;
@@ -88,19 +95,19 @@ export default class HtmlRenderer {
             }
 
             return (
-              <View key={index} style={{width: 330, flexDirection: 'row', flexWrap: 'wrap'}} >
+              <ContainerElement key={index} style={{ width: 330, flexDirection: 'row', flexWrap: 'wrap' }} >
                 {domToElement(node.children, node)}
                 {paragraphBreak}
-              </View>
+              </ContainerElement>
             );
           } else if (node.name === 'h1' || node.name === 'h2' || node.name === 'h3' || node.name === 'h4' || node.name === 'h5') {
             return (
-              <View key={index} >
+              <ContainerElement key={index} >
                 {domToElement(node.children, node)}
                 <Text>{PARAGRAPH_BREAK}</Text>
-              </View>
+              </ContainerElement>
             );
-          } else {
+          } else if (node.name === 'attachment') {
             return (
               <View key={index} >
                 {mediaTag}
