b2aaa342e5db4b979e014df9e331861c01dfa041
Vladimir Adamic <vladimir@shoutem.com>
2016-04-29 10:46:25 +0200

    Rename HTMLView file to CompositeMedia

--- extensions/shoutem.ui/components/CompositeMediaComponent/lib/AttachmentTagTransformer.js
+++ extensions/shoutem.ui/components/CompositeMediaComponent/lib/AttachmentTagTransformer.js
@@ -1,6 +1,10 @@
-import React from 'react-native';
+import React, {
+  Dimensions,
+} from 'react-native';
 import ImagePreview from '../../ImagePreview';
 import Video from '../../Video/Video';
+
+const {height, width} = Dimensions.get('window');
 
 class AttachmentTagTransformer {
   canHandle(node) {
@@ -18,11 +22,14 @@ class AttachmentTagTransformer {
       // TODO(Vladimir) - ensure images exists
       const image = this.attachments.images.find((image) => image.id === id);
 
+      const imageWidth = (width < image.width) ? width : image.width;
+      const imageScale = imageWidth/image.width;
+
       return (
         <ImagePreview
           source={{ uri: image.src }}
-          width={image.width}
-          height={image.height}
+          width={imageWidth - 30}
+          height={image.height - 30}
           key={key}
         />
       );
@@ -31,12 +38,14 @@ class AttachmentTagTransformer {
     if (node.attribs.type === 'video') {
       // TODO(Vladimir) - ensure images exists
       const video = this.attachments.videos.find((video) => video.id === id);
+      const videoWidth = (width < video.width) ? width : video.width;
+      const heightScale = videoWidth/video.width
 
       return (
         <Video
           source={video.src}
-          width={video.width}
-          height={video.height}
+          width={videoWidth - 30}
+          height={video.height * heightScale - 30}
           key={key}
         />
       );
