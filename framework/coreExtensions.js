2a0d9f8c406f5cf90782ff69c213a2ebb78d59d6
Ivan Vuković <3.14wee@gmail.com>
2016-06-14 11:42:08 +0200

    Publish framework on npm as @shoutem/core

--- framework/coreExtensions.js
+++
@@ -1,9 +0,0 @@
-import coreReducer from './coreReducer';
-import devEnvironment from './devEnvironment';
-
-export default {
-  'shoutem.core': {
-    reducer: coreReducer,
-    middleware: devEnvironment.getReduxMiddleware(),
-  },
-};
