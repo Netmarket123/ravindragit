6205632296cc60f94ebf7c66eb4cd24ffdb2d611
Ivan Vuković <3.14wee@gmail.com>
2016-07-01 14:15:17 +0200

    SEEXT-730 - Default action from shortcut settings 	* support screen in execute shortcut

--- extensions/shoutem.application/middleware.js
+++ extensions/shoutem.application/middleware.js
@@ -1,4 +1,5 @@
 import * as _ from 'lodash';
+import { navigateTo } from '@shoutem/core/navigation';
 
 /**
  * Creates screen settings for given shortcut.
@@ -17,20 +18,37 @@ function createScreenSettings(shortcut) {
 export function createExecuteShortcutMiddleware(actions) {
   return store => next => action => {
     const actionName = _.get(action, 'shortcut.attributes.action');
+    const screenName = _.get(action, 'shortcut.attributes.screen');
 
-    if (!actionName) {
+    if (!actionName && !screenName) {
       return next(action);
     }
 
     const settings = createScreenSettings(action.shortcut);
     const children = _.get(action.shortcut, 'relationships.children.data');
-    const shortcutAction = actions[actionName];
+    if (actionName) {
+      const shortcutAction = actions[actionName];
 
-    if (typeof shortcutAction === 'function') {
-      return next(shortcutAction(settings, children, store.getState()));
+      if (typeof shortcutAction === 'function') {
+        return next(shortcutAction(settings, children, store.getState()));
+      }
+
+      throw new Error(`Shortcut you tried to execute has no valid action (${actionName}),
+      or you tried to execute shortcut before appDidMount. Check exports of your extension.`);
     }
 
-    throw new Error(`Shortcut you tried to execute has no valid action (${actionName}),
-  or you tried to execute shortcut before appDidMount. Check exports of your extension.`);
+    if (screenName) {
+      const openScreenAction = () =>
+        navigateTo({
+          screen: screenName,
+          props: {
+            children,
+            ...settings,
+          },
+        });
+
+      // No need for error handling, if screen is invalid navigate to will throw Exception
+      return next(openScreenAction());
+    }
   };
 }
