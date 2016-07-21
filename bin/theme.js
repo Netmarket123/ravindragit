f0ae451314a2a30a205722ec057ba80e321f8450
Belma <belma.dzi@gmail.com>
2016-07-21 15:38:06 +0200

    Small changes replacing padding + navigation bar transparency

--- bin/theme.js
+++ bin/theme.js
@@ -722,7 +722,6 @@ export default () => ({
     },
     container: {
       margin: MEDIUM_GUTTER,
-      paddingVertical: MEDIUM_GUTTER,
     },
   },
 
@@ -970,8 +969,8 @@ export default () => ({
     '.section-header': {
       'shoutem.ui.View': {
         'shoutem.ui.Caption': {
-          paddingVertical: SMALL_GUTTER,
-          paddingHorizontal: MEDIUM_GUTTER,
+          marginVertical: SMALL_GUTTER,
+          marginHorizontal: MEDIUM_GUTTER,
           color: '#888888',
         },
       },
