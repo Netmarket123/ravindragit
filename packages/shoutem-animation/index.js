c9e422cadda3e1ee47879778d90ce4dd7194bcc3
Ivan Vuković <3.14wee@gmail.com>
2016-08-04 14:38:08 +0200

    Revert "Feature/hero image animation"

--- packages/shoutem-animation/index.js
+++ packages/shoutem-animation/index.js
@@ -1,9 +1,2 @@
 export { default as makeZoomable } from './ZoomableComponent';
 export { ScrollDriver } from './ScrollDriver';
-export { Parallax } from './Parallax';
-export { ZoomIn } from './ZoomIn';
-export { ZoomOut } from './ZoomOut';
-export { FadeIn } from './FadeIn';
-export { FadeOut } from './FadeOut';
-
-export { HeroHeader } from './HeroHeader';
