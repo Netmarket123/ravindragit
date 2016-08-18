6adfafbdbcbea7d75f6376d63eb18b1ab861abc7
MrBr <lbracano@gmail.com>
2016-08-18 23:07:00 +0200

    Implement & use getSelectedLayoutScreen
    - Execute shortcut now search for proper screen from screens prop

--- extensions/shoutem.application/middleware.js
+++ extensions/shoutem.application/middleware.js
@@ -21,6 +21,18 @@ function createScreenSettings(shortcut) {
   };
 }
 
+/**
+ * Find selected screen layout for screenType.
+ * Used to get proper screen when navigating to specific screen type.
+ * Screen layouts are selected for shortcut in builder.
+ * @param screens
+ * @param screenType
+ * @returns {V}
+ */
+function getSelectedLayoutScreen(screens, screenType) {
+  return _.get(screens.find(screen => screen.canonicalType === screenType), 'canonicalName');
+}
+
 function getChildShortcuts(store, parentShortcut) {
   const childrenDescriptors = _.get(parentShortcut, 'relationships.children.data', []);
   const shortcuts = _.get(store.getState(), ['shoutem.application', 'shortcuts']);
@@ -85,7 +97,9 @@ const selectScreenLayout = store => next => action => {
 // eslint-disable-next-line no-unused-vars
 const navigateToShortcutScreen = store => next => action => {
   if (action.type === EXECUTE_SHORTCUT) {
-    const screenName = _.get(action, 'shortcut.attributes.screen');
+    const screenType = _.get(action, 'shortcut.attributes.screen');
+    const screens = _.get(action, 'shortcut.attributes.screens', []);
+    const screenName = getSelectedLayoutScreen(screens, screenType) || screenType;
     const settings = createScreenSettings(action.shortcut);
     const children = getChildShortcuts(store, action.shortcut);
     const navigateAction = navigation[action.navigationAction];
