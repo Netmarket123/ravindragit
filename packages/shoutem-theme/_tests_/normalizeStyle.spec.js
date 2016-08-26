ba6b44f1505521b233fb77515009e00ee86be927
MrBr <lbracano@gmail.com>
2016-08-26 15:40:23 +0200

    Change tests

--- packages/shoutem-theme/_tests_/normalizeStyle.spec.js
+++ packages/shoutem-theme/_tests_/normalizeStyle.spec.js
@@ -1,27 +1,26 @@
 import { assert } from 'chai';
-import normalizeStyle from '../src/StyleNormalizer/normalizeStyle';
+import ShorthandsNormalizerFactory, {
+  ALL_CORNERS,
+  ALL_SIDES,
+  HORIZONTAL,
+  VERTICAL,
+} from '../src/StyleNormalizer/ShorthandsNormalizerFactory';
 
 // We can not guarantee order when shorthands are mixed
 // with single values or other shorthands
 
+const TEST_VAL = 5;
 describe('normalizeStyle', () => {
-  it('normalize padding', () => {
-    const style = {
-      style: {
-        padding: 5,
-      },
-    };
-
+  it('normalize all sides', () => {
+    const normalizePropVal = ShorthandsNormalizerFactory.create('padding', ALL_SIDES);
     const expectedStyle = {
-      style: {
-        paddingTop: 5,
-        paddingLeft: 5,
-        paddingBottom: 5,
-        paddingRight: 5,
-      },
+      paddingTop: TEST_VAL,
+      paddingLeft: TEST_VAL,
+      paddingBottom: TEST_VAL,
+      paddingRight: TEST_VAL,
     };
 
-    const normalizedStyle = normalizeStyle(style);
+    const normalizedStyle = normalizePropVal(TEST_VAL);
 
     assert.deepEqual(
       normalizedStyle,
@@ -29,21 +28,16 @@ describe('normalizeStyle', () => {
       'style not normalized correctly'
     );
   });
-  it('normalize marginHorizontal', () => {
-    const style = {
-      style: {
-        marginHorizontal: 5,
-      },
-    };
-
+  it('normalize all sides with suffix', () => {
+    const normalizePropVal = ShorthandsNormalizerFactory.create('padding', ALL_SIDES, 'Suffix');
     const expectedStyle = {
-      style: {
-        marginLeft: 5,
-        marginRight: 5,
-      },
+      paddingTopSuffix: TEST_VAL,
+      paddingLeftSuffix: TEST_VAL,
+      paddingBottomSuffix: TEST_VAL,
+      paddingRightSuffix: TEST_VAL,
     };
 
-    const normalizedStyle = normalizeStyle(style);
+    const normalizedStyle = normalizePropVal(TEST_VAL);
 
     assert.deepEqual(
       normalizedStyle,
@@ -51,23 +45,16 @@ describe('normalizeStyle', () => {
       'style not normalized correctly'
     );
   });
-  it('normalize borderRadius', () => {
-    const style = {
-      style: {
-        borderRadius: 5,
-      },
-    };
-
+  it('normalize all corners', () => {
+    const normalizePropVal = ShorthandsNormalizerFactory.create('border', ALL_CORNERS);
     const expectedStyle = {
-      style: {
-        borderBottomLeftRadius: 5,
-        borderBottomRightRadius: 5,
-        borderTopLeftRadius: 5,
-        borderTopRightRadius: 5,
-      },
+      borderBottomLeft: TEST_VAL,
+      borderBottomRight: TEST_VAL,
+      borderTopLeft: TEST_VAL,
+      borderTopRight: TEST_VAL,
     };
 
-    const normalizedStyle = normalizeStyle(style);
+    const normalizedStyle = normalizePropVal(TEST_VAL);
 
     assert.deepEqual(
       normalizedStyle,
@@ -75,23 +62,16 @@ describe('normalizeStyle', () => {
       'style not normalized correctly'
     );
   });
-  it('normalize borderWidth', () => {
-    const style = {
-      style: {
-        borderWidth: 5,
-      },
-    };
-
+  it('normalize all corners with suffix', () => {
+    const normalizePropVal = ShorthandsNormalizerFactory.create('border', ALL_CORNERS, 'Radius');
     const expectedStyle = {
-      style: {
-        borderLeftWidth: 5,
-        borderRightWidth: 5,
-        borderBottomWidth: 5,
-        borderTopWidth: 5,
-      },
+      borderBottomLeftRadius: TEST_VAL,
+      borderBottomRightRadius: TEST_VAL,
+      borderTopLeftRadius: TEST_VAL,
+      borderTopRightRadius: TEST_VAL,
     };
 
-    const normalizedStyle = normalizeStyle(style);
+    const normalizedStyle = normalizePropVal(TEST_VAL);
 
     assert.deepEqual(
       normalizedStyle,
@@ -99,23 +79,14 @@ describe('normalizeStyle', () => {
       'style not normalized correctly'
     );
   });
-  it('normalize borderColor', () => {
-    const style = {
-      style: {
-        borderColor: 5,
-      },
-    };
-
+  it('normalize horizontal', () => {
+    const normalizePropVal = ShorthandsNormalizerFactory.create('margin', HORIZONTAL);
     const expectedStyle = {
-      style: {
-        borderLeftColor: 5,
-        borderRightColor: 5,
-        borderBottomColor: 5,
-        borderTopColor: 5,
-      },
+      marginLeft: TEST_VAL,
+      marginRight: TEST_VAL,
     };
 
-    const normalizedStyle = normalizeStyle(style);
+    const normalizedStyle = normalizePropVal(TEST_VAL);
 
     assert.deepEqual(
       normalizedStyle,
@@ -123,55 +94,14 @@ describe('normalizeStyle', () => {
       'style not normalized correctly'
     );
   });
-  it('nested style', () => {
-    const style = {
-      button: {
-        paddingVertical: 6,
-        marginTop: 5,
-        marginHorizontal: 7,
-        borderRadius: 5,
-      },
-      card: {
-        padding: 10,
-        overlay: {
-          paddingHorizontal: 5,
-          paddingVertical: 10,
-          borderWidth: 5,
-        },
-      },
-    };
-
+  it('normalize vertical', () => {
+    const normalizePropVal = ShorthandsNormalizerFactory.create('margin', VERTICAL);
     const expectedStyle = {
-      button: {
-        paddingBottom: 6,
-        paddingTop: 6,
-        marginTop: 5,
-        marginRight: 7,
-        marginLeft: 7,
-        borderBottomLeftRadius: 5,
-        borderBottomRightRadius: 5,
-        borderTopLeftRadius: 5,
-        borderTopRightRadius: 5,
-      },
-      card: {
-        paddingTop: 10,
-        paddingBottom: 10,
-        paddingLeft: 10,
-        paddingRight: 10,
-        overlay: {
-          paddingRight: 5,
-          paddingLeft: 5,
-          paddingTop: 10,
-          paddingBottom: 10,
-          borderLeftWidth: 5,
-          borderRightWidth: 5,
-          borderTopWidth: 5,
-          borderBottomWidth: 5,
-        },
-      },
+      marginTop: TEST_VAL,
+      marginBottom: TEST_VAL,
     };
 
-    const normalizedStyle = normalizeStyle(style);
+    const normalizedStyle = normalizePropVal(TEST_VAL);
 
     assert.deepEqual(
       normalizedStyle,
