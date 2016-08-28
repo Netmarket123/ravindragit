86282255aa2e5051e6586d0c7a170e0367610d57
Ivan Vuković <3.14wee@gmail.com>
2016-08-29 00:19:53 +0200

    Create examples for all animations

--- packages/shoutem-animation/Example/ShoutemAnimation/Parallax.js
+++ packages/shoutem-animation/Example/ShoutemAnimation/Parallax.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
-import { ScrollView, ListView } from 'react-native';
+import { ScrollView } from 'react-native';
 
 import {
   Parallax,
-  HeroHeader,
   ScrollDriver,
 } from '@shoutem/animation';
 
@@ -23,49 +22,7 @@ export default class ParallaxExample extends Component {
   }
 
   getRestaurants() {
-    return [{
-      "name": "Gaspar Brasserie",
-      "address": "185 Sutter St, San Francisco, CA 94109",
-      "description": "Expect an intimate venue with the ambience of a private club. The mood is casual, the guests sublime.",
-      "url": "gasparbrasserie.com",
-      "image": { "url": "https://shoutem.github.io/restaurants/restaurant-1.jpg"},
-      "mail": "info@gasparbrasserie.com"
-    }, {
-      "name": "Chalk Point Kitchen",
-      "address": "527 Broome St, New York, NY 10013",
-      "description": "",
-      "url": "",
-      "image": { "url": "https://shoutem.github.io/restaurants/restaurant-2.jpg"},
-      "mail": ""
-    }, {
-      "name": "Kyoto Amber Upper East",
-      "address": "225 Mulberry St, New York, NY 10012",
-      "description": "",
-      "url": "",
-      "image": { "url": "https://shoutem.github.io/restaurants/restaurant-3.jpg"},
-      "mail": ""
-    }, {
-      "name": "Sushi Academy",
-      "address": "",
-      "description": "",
-      "url": "",
-      "image": { "url": "https://shoutem.github.io/restaurants/restaurant-4.jpg"},
-      "mail": ""
-    }, {
-      "name": "Sushibo",
-      "address": "",
-      "description": "",
-      "url": "",
-      "image": { "url": "https://shoutem.github.io/restaurants/restaurant-5.jpg"},
-      "mail": ""
-    }, {
-      "name": "Mastergrill",
-      "address": "",
-      "description": "",
-      "url": "",
-      "image": { "url": "https://shoutem.github.io/restaurants/restaurant-6.jpg"},
-      "mail": ""
-    }];
+    return require('./restaurants.json');
   }
 
   renderRow(restaurant) {
