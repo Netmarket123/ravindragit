21dcc70082293a1f3f9882d35bbf8b57c45a569b
MrBr <lbracano@gmail.com>
2016-08-29 21:52:34 +0200

    Add StyleNormalize tests

---
+++ packages/shoutem-theme/_tests_/StyleNormalizer.spec.js
@@ -0,0 +1,48 @@
+import { assert } from 'chai';
+import StyleNormalizer from '../src/StyleNormalizer/StyleNormalizer';
+import {
+  SIDES,
+  CORNERS,
+  HORIZONTAL,
+  VERTICAL,
+} from '../src/StyleNormalizer/ShorthandsNormalizerFactory';
+
+// We can not guarantee order when shorthands are mixed
+// with single values or other shorthands
+
+describe('StyleNormalizer', () => {
+  describe('shorthand normalizers creation', () => {
+    it('creates proper sides normalizers', () => {
+      const styleNormalizer = new StyleNormalizer();
+      styleNormalizer.createNormalizers('test', [SIDES]);
+
+      assert.isOk(styleNormalizer.normalizers.test, 'style not normalized correctly');
+    });
+    it('creates proper horizontal normalizers', () => {
+      const styleNormalizer = new StyleNormalizer();
+      styleNormalizer.createNormalizers('test', [HORIZONTAL]);
+
+      assert.isOk(styleNormalizer.normalizers.testHorizontal, 'style not normalized correctly');
+    });
+    it('creates proper vertical normalizers', () => {
+      const styleNormalizer = new StyleNormalizer();
+      styleNormalizer.createNormalizers('test', [VERTICAL]);
+
+      assert.isOk(styleNormalizer.normalizers.testVertical, 'style not normalized correctly');
+    });
+    it('creates proper horizontal normalizers', () => {
+      const styleNormalizer = new StyleNormalizer();
+      styleNormalizer.createNormalizers('test', [CORNERS]);
+
+      assert.isOk(styleNormalizer.normalizers.test, 'style not normalized correctly');
+    });
+  });
+  describe('normalizers creation with suffix', () => {
+    it('creates proper normalizers with suffix', () => {
+      const styleNormalizer = new StyleNormalizer();
+      styleNormalizer.createNormalizers('test', [SIDES], 'Suffix');
+
+      assert.isOk(styleNormalizer.normalizers.testSuffix, 'style not normalized correctly');
+    });
+  });
+});
