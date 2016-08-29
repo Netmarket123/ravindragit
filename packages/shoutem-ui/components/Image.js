c458fd045f5f146c07ef3c8dd644f36c15ee97db
Ivan Vuković <3.14wee@gmail.com>
2016-08-29 23:33:15 +0200

    connect Image with animations

--- packages/shoutem-ui/components/Image.js
+++ packages/shoutem-ui/components/Image.js
@@ -1,59 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {
   Image as RNImage,
-  Animated as RNAnimated,
   Platform,
 } from 'react-native';
 
 import { connectStyle } from '@shoutem/theme';
+import { connectAnimation } from '@shoutem/animation';
 
-function Image(props) {
-  let resolvedProps = props;
-  const { source, defaultSource } = props;
-
-  // defaultSource is not supported on Android, so we manually
-  // reassign the defaultSource prop to source if source is not defined
-  if ((Platform.OS === 'android') && (!source || !source.uri)) {
-    resolvedProps = {
-      ...props,
-      // Image views are not rendered on Android if there is no image to display,
-      // so we fallback to a transparent image to be compatible with iOS
-      source: defaultSource || require('../assets/images/transparent.png'),
-      // Fixes a bug with local image resizing on Android:
-      // https://github.com/facebook/react-native/issues/4598#issuecomment-162328501
-      style: { ...props.style, width: null },
-    };
+class Image extends Component {
+  static propTypes = {
+    ...RNImage.propTypes,
   }
 
-  return (
-    <RNImage {...resolvedProps}>
-      {props.children}
-    </RNImage>
-  );
+  render() {
+    const { props } = this;
+    let resolvedProps = props;
+    const { source, defaultSource } = props;
+
+    // defaultSource is not supported on Android, so we manually
+    // reassign the defaultSource prop to source if source is not defined
+    if ((Platform.OS === 'android') && (!source || !source.uri)) {
+      resolvedProps = {
+        ...props,
+        // Image views are not rendered on Android if there is no image to display,
+        // so we fallback to a transparent image to be compatible with iOS
+        source: defaultSource || require('../assets/images/transparent.png'),
+        // Fixes a bug with local image resizing on Android:
+        // https://github.com/facebook/react-native/issues/4598#issuecomment-162328501
+        style: { ...props.style, width: null },
+      };
+    }
+
+    return (
+      <RNImage {...resolvedProps}>
+        {props.children}
+      </RNImage>
+    );
+  }
 }
 
-Image.propTypes = {
-  ...RNImage.propTypes,
-};
+const AnimatedImage = connectAnimation(Image);
 
-function AnimatedImage(props) {
-  return (
-    <RNAnimated.Image {...props}>
-      {props.children}
-    </RNAnimated.Image>
-  );
-}
-
-AnimatedImage.propTypes = {
-  ...RNImage.propTypes,
-};
-
-const Animated = {
-  Image: connectStyle('shoutem.ui.Animated.Image', {})(AnimatedImage),
-};
-
-const StyledImage = connectStyle('shoutem.ui.Image', {})(Image);
+const StyledImage = connectStyle('shoutem.ui.Image', {})(AnimatedImage);
 export {
   StyledImage as Image,
-  Animated,
 };
