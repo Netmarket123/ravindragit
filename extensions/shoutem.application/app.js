d9f1d10fafd6a48c69c368662f9e19d232c164f2
MrBr <lbracano@gmail.com>
2016-09-09 15:11:02 +0200

    Code formatting

--- extensions/shoutem.application/app.js
+++ extensions/shoutem.application/app.js
@@ -1,9 +1,9 @@
-export const appActions = {};
-
 import { extractAppActions } from './shared/extractAppActions';
 import { setActiveTheme } from './shared/setActiveTheme';
 import { watchActiveTheme } from './shared/watchActiveTheme';
 import { registerConfigurationSchema } from './shared/registerConfigurationSchema';
+
+export const appActions = {};
 
 export function appWillMount(app) {
   registerConfigurationSchema(app.getAppId());
