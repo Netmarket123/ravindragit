d61defea1d49a4af42a79ad784f45691f2b9951e
Ivan Vuković <3.14wee@gmail.com>
2016-09-16 14:05:36 +0200

    Upgrade react-native to 0.33

--- ios/ShoutemApp/AppDelegate.m
+++ ios/ShoutemApp/AppDelegate.m
@@ -8,52 +8,29 @@
  */
 
 #import "AppDelegate.h"
-
-#import "RCTRootView.h"
-
-#import "RCTLinkingManager.h"
-
 #import "CodePush.h"
+
+#import "RCTBundleURLProvider.h"
+#import "RCTRootView.h"
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   NSURL *jsCodeLocation;
-  #ifdef DEBUG
-  /**
-   * Loading JavaScript code - uncomment the one you want.
-   *
-   * OPTION 1
-   * Load from development server. Start the server from the repository root:
-   *
-   * $ npm start
-   *
-   * To run on device, change `localhost` to the IP address of your computer
-   * (you can get this by typing `ifconfig` into the terminal and selecting the
-   * `inet` value under `en0:`) and make sure your computer and iOS device are
-   * on the same Wi-Fi network.
-   */
 
-    jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.ios.bundle?platform=ios&dev=true"];
-  #else
-  /**
-   * OPTION 2
-   * Configures the app to always load the most recent version of its JS bundle.
-   * On the first launch, this will correspond to the file that was compiled
-   * with the app. However, after an update has been pushed via CodePush, this
-   * will return the location of the most recently installed update.
-   *
-   * NOTE: app's JS bundle must be named main.jsbundle
-   */
-
+  
+#ifdef DEBUG
+    jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
+#else
     jsCodeLocation = [CodePush bundleURL];
-  #endif
+#endif
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"ShoutemApp"
                                                initialProperties:nil
                                                    launchOptions:launchOptions];
+  rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
 
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
@@ -61,13 +38,6 @@
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
   return YES;
-}
-
-- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url
-  sourceApplication:(NSString *)sourceApplication annotation:(id)annotation
-{
-  return [RCTLinkingManager application:application openURL:url
-                      sourceApplication:sourceApplication annotation:annotation];
 }
 
 @end
