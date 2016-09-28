e7215ae0881a753a501c167f4f41d495a858ecc7
Ivan Vuković <3.14wee@gmail.com>
2016-09-28 17:24:22 +0200

    SEEXT-1385 - Navigation bar icons missing, question marks displayed instead

--- ios/ShoutemApp/AppDelegate.m
+++ ios/ShoutemApp/AppDelegate.m
@@ -21,11 +21,11 @@
   NSURL *jsCodeLocation;
 
   
-//#ifdef DEBUG
+#ifdef DEBUG
     jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
-//#else
-//    jsCodeLocation = [CodePush bundleURL];
-//#endif
+#else
+    jsCodeLocation = [CodePush bundleURL];
+#endif
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"ShoutemApp"
