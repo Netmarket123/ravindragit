688d029053da1862058ef6c5acfb0cf1377c87a6
Vladimir Adamic <vladimir@shoutem.com>
2016-10-28 15:56:52 +0200

    Add BackgroundNotificationReceiver for notications

---
+++ android/app/src/main/java/com/shoutemapp/BackgroundNotificationReceiver.java
@@ -0,0 +1,88 @@
+package com.shoutemapp;
+
+import android.annotation.SuppressLint;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+import com.google.firebase.messaging.RemoteMessage;
+
+import java.util.Map;
+
+public class BackgroundNotificationReceiver extends BroadcastReceiver {
+    private static final String TAG = "BackgroundNotificationReceiver";
+    @SuppressLint("LongLogTag")
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String action = intent.getAction();
+
+        Log.d(TAG, "Remote message received for action " + action);
+
+        if (action.equals("com.evollu.react.fcm.ReceiveNotification")) {
+           Object data = intent.getExtras().getParcelable("data");
+
+            if (data instanceof RemoteMessage) {
+                RemoteMessage remoteMessage = (RemoteMessage) data;
+                String title;
+                String text;
+                String id;
+
+                if (remoteMessage.getNotification() != null) {
+                    title = remoteMessage.getNotification().getTitle();
+                    text = remoteMessage.getNotification().getBody();
+                    id = remoteMessage.getMessageId();
+                } else {
+                    title = remoteMessage.getData().get("title");
+                    text = remoteMessage.getData().get("text");
+                    id = remoteMessage.getData().get("id");
+                }
+
+                Log.d(TAG, "From: " + remoteMessage.getFrom());
+                Log.d(TAG, "Notification Message id: " + id);
+                Log.d(TAG, "Notification Message Title: " + title);
+                Log.d(TAG, "Notification Message Body/Text: " + text);
+                Log.d(TAG, "Remote message received" + remoteMessage.toString());
+
+                sendNotification(context, id, title, text, remoteMessage.getData());
+            }
+        }
+    }
+
+    private void sendNotification(Context context,String id, String title, String messageBody, Map<String, String> data) {
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+        //PendingIntent pendingIntent = PendingIntent.getBroadcast(context, id.hashCode(), intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationManager notificationManager =
+                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        NotificationCompat.Builder notificationBuilder;
+
+        if (title != null && messageBody != null && id !=null && data != null) {
+            notificationBuilder = new NotificationCompat.Builder(context)
+                    .setSmallIcon(context.getApplicationInfo().icon)
+                    .setContentTitle(title)
+                    .setContentText(messageBody)
+                    .setStyle(new NotificationCompat.BigTextStyle().bigText(messageBody))
+                    .setAutoCancel(true)
+                    .setSound(defaultSoundUri);
+            //.setContentIntent(pendingIntent);
+        } else {
+            notificationBuilder = new NotificationCompat.Builder(context)
+                    .setSmallIcon(context.getApplicationInfo().icon)
+                    .setContentTitle("Invalid title")
+                    .setContentText("Invalid text")
+                    .setStyle(new NotificationCompat.BigTextStyle().bigText(messageBody))
+                    .setAutoCancel(true)
+                    .setSound(defaultSoundUri);
+        }
+
+        notificationManager.notify(notificationBuilder.hashCode(), notificationBuilder.build());
+    }
+}
