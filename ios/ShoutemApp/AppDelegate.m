4f216e0a4812af4438b8209ccd2208fbccd86f08
Ivan Vuković <3.14wee@gmail.com>
2016-11-10 18:15:05 +0100

    Fix RCTJSONStringify() exception

--- ios/ShoutemApp/AppDelegate.m
+++ ios/ShoutemApp/AppDelegate.m
@@ -21,7 +21,7 @@
   NSURL *jsCodeLocation;
 
   // Appetizer.io params check
-  NSDictionary *initialProperties = [[NSUserDefaults standardUserDefaults] dictionaryRepresentation];
+  NSDictionary *initialProperties = [[NSUserDefaults standardUserDefaults] dictionaryForKey:@"initialProps"];
   
 #ifdef DEBUG
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
@@ -31,7 +31,7 @@
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"ShoutemApp"
-                                               initialProperties:initialProperties
+                                               initialProperties: initialProperties
                                                    launchOptions:launchOptions];
   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
 
