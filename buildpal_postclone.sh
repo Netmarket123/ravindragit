c161bc131e0970193455b862d950205eeced7680
Damir Juretic <Damir Juretic>
2016-11-21 14:43:17 +0100

    support build type (debug, release)

--- buildpal_postclone.sh
+++ buildpal_postclone.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
+file=$(cat config.template.json)
+
 # update NID of the application
 if [ "$NID" != '' ]
 then
-  sed "s/2080/$NID/g" config.template.json > config.json
-  echo "Application id is updated to $NID"
+  tmp=$(echo "$file" | sed "s/2080/$NID/g")
+  file=$tmp
+  echo "Application id is updated to $NID."
 fi
+
+# update build type to production
+if [ "$BUILD_TYPE" = 'Release' ]
+then
+  tmp=$(echo "$file" | sed 's/"production".*/"production": true,/g')
+  file=$tmp
+  echo "Changed build type to release."
+fi
+
+echo $file > config.json
 
 # copy package.template.json to package.json
 cp package.template.json package.json
